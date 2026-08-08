49cd1145f77482f64d8b9e83a68365c75984ceab
Youssif Hany <youssifhany962@gmail.com>
2026-08-08 14:04:34 +0300

    Making RoutingView public

--- Sources/SwiftNavigation/RoutingView.swift
+++ Sources/SwiftNavigation/RoutingView.swift
@@ -7,17 +7,17 @@
 
 import SwiftUI
 
-struct RoutingView<Root: View, Routes: Routable>: View {
+public struct RoutingView<Root: View, Routes: Routable>: View {
     @Binding private var routes: [Routes]
     private let root: () -> Root
-    
+
     public init(routes: Binding<[Routes]>,
                 @ViewBuilder root: @escaping () -> Root) {
         self._routes = routes
         self.root = root
     }
 
-    var body: some View {
+    public var body: some View {
         NavigationStack(path: $routes) {
             root()
                 .navigationDestination(for: Routes.self) { view in
